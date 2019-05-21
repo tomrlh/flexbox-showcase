541f6c8df02ce2e17dd008231f0f6d559673726a
Thiago Medeiros <si-thiago@hotmail.com>
2019-05-20 22:34:16 -0300

    type: update
    Flexbox demonstration working almost perfect :open_hands:

--- App.js
+++ App.js
@@ -4,21 +4,32 @@ import { StyleSheet, Text, View, AppRegistry, Picker } from 'react-native'
 class FlexboxExamples extends Component {
   state = {
     justifyContent: 'flex-start',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    alignItems: 'flex-start'
   }
 
-
+  getStyles = () => {
+    return StyleSheet.create({
+      container: {
+        flex: 1,
+        flexDirection: this.state.flexDirection,
+        justifyContent: this.state.justifyContent,
+        alignItems: this.state.alignItems
+      },
+      box: {
+        height: 50,
+        width: 50,
+        backgroundColor: '#e76e63',
+        margin: 10,
+      }
+    })
+  }
 
   render() {
-    const containerStyle = {
-      flex: 1,
-      flexDirection: this.state.flexDirection,
-      justifyContent: this.state.justifyContent
-    }
-
     return (
       <View>
-        <View style={styles.header}>
+        <View>
+        <Text>Justify Content</Text>
           <Picker
             selectedValue={this.state.justifyContent}
             onValueChange={(selectedValue) => this.setState({justifyContent: selectedValue})}>
@@ -28,6 +39,18 @@ class FlexboxExamples extends Component {
              <Picker.Item label="Space Around" value="space-around" />
              <Picker.Item label="Center" value="center" />
           </Picker>
+          
+          <Text>Align Items</Text>
+          <Picker
+            selectedValue={this.state.alignItems}
+            onValueChange={(selectedValue) => this.setState({alignItems: selectedValue})}>
+             <Picker.Item label="Flex Start" value="flex-start" />
+             <Picker.Item label="Flex End" value="flex-end" />
+             <Picker.Item label="Center" value="center" />
+             <Picker.Item label="Stretch" value="stretch" />
+          </Picker>
+
+          <Text>Flex Direction</Text>
           <Picker
             selectedValue={this.state.flexDirection}
             onValueChange={(selectedValue) => this.setState({flexDirection: selectedValue})}>
@@ -36,23 +59,14 @@ class FlexboxExamples extends Component {
           </Picker>
         </View>
 
-        <View style={containerStyle}>
-          <View style={styles.box}/>
-          <View style={styles.box}/>
-          <View style={styles.box}/>
+        <View style={this.getStyles().container}>
+          <View style={this.getStyles().box}/>
+          <View style={this.getStyles().box}/>
+          <View style={this.getStyles().box}/>
         </View>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  box: {
-    height: 50,
-    width: 50,
-    backgroundColor: '#e76e63',
-    margin: 10,
-  }
-})
 
 export default FlexboxExamples;
